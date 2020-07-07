a020ec8e33714cb02647b98f0597f955a5bbd7a6
Rithika-Raj <rithuraj08@gmail.com>
2020-07-07 18:15:27 +0530

    moved api into src

---
+++ src/api/src/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    jwtSecret: 'long-live-the-ionic-academy',
+
+    db: 'mongodb+srv://geektest:geektestrithu@cluster0-z8izz.mongodb.net/CrudDB?retryWrites=true&w=majority', 
+    
+    //db: 'mongodb://localhost/ionic-jwt' ,
+    //useUnifiedTopology: true 
+};
