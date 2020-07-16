0b31d24d30e0db35d17e12fc8042a0d3b8d5b9c1
Rithika-Raj <rithuraj08@gmail.com>
2020-07-16 11:45:52 +0530

    good day

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url:'https://iontestapp.herokuapp.com/home'
+  url:'https://iontestapp.herokuapp.com'
   //url: 'http://localhost:5000',
   //port: process.env.PORT || 3000
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url:'https://iontestapp.herokuapp.com/home'
+  url:'https://iontestapp.herokuapp.com'
 };
 
 /*
