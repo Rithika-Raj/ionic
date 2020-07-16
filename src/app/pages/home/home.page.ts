db3f3f562be7d493b4d2dc7128c965a3a174ac5f
Rithika-Raj <rithuraj08@gmail.com>
2020-07-16 12:27:41 +0530

    good day

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -8,8 +8,8 @@ import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 
 export class HomePage implements OnInit {
